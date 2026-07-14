7715f0f8a72f291efff617ab641e80dfd70fe651
Regissalt <thiangabrielregis@gmail.com>
2026-07-14 14:20:26 -0300

    Fix ruta templates

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+# Definimos la ruta absoluta por seguridad
+template_dir = os.path.abspath('templates')
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
